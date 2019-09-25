aef81c6d5299860b835a6ac74137dd852bc60581
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-09-25 13:07:35 -0400

    More options in createmodel()

--- enigmalibrary/enigmamodels.py
+++ enigmalibrary/enigmamodels.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*-
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
-from keras.activations import relu, softmax
-from keras.optimizers import Adam, Nadam
-from keras.callbacks import EarlyStopping
-from keras_tqdm import TQDMNotebookCallback
-import talos as ta
-from talos.metrics.keras_metrics import fmeasure_acc
 
-def createmodel(optimizer='adam',
-                activation='relu',
-                initmode='uniform',
-                dropout=0.2):
+def CreateSequentialModel(input_dim=None,
+                          optimizer='Adam',
+                          learnrate=0.1,
+                          activation='relu',
+                          initmode='uniform',
+                          dropout=0.2,
+                          units_L1=5,
+                          units_L2=10,
+                          L2_reg_penalty=0.01):
     """Constructs Sequential NN model based on Talos optimization parameters
 
     Inputs
     ------
     optimizer:  string | Default: 'adam'
                 optimizer to use for deep learning model
+
+    learnrate:  float between 0 and 1 | Default: 0.1
+                learning rate of optimizer
 
     activation: string | Default: 'relu'
                 activation to use for deep learning model
@@ -29,20 +31,36 @@ def createmodel(optimizer='adam',
     dropout:    float between 0 and 1 | Default: 0.2
                 Dropout rate to apply to layer input
 
+    units_L1:   int >= 0 | Default: 5
+                number of units in first dense layer
+
+    units_L2:   int >= 0 | Default: 10
+                number of units in second dense layer
+
+    L2_penalty: float between 0 and 1 | Default: 0.01
+                penalty on second dense layer's kernel regularizer
+
     Returns
     -------
     model:      sequential model based on input parameters
     """
-
+    if inputdim is None:
+        raise Exception('Please enter number of input dimensions to '
+                        'initialize a Sequential model')
     model = Sequential()
-    model.add(Dense(64,
+    model.add(Dense(input_dim=input_dim,
+                    units=units_L1,
                     activation=activation,
-                    kernel_initializer=kernel_initializer))
+                    kernel_initializer=initmode))
     model.add(Dropout(dropout))
-    model.add(Dense(1,activation=activation,
-                    kernel_initializer=kernel_initializer))
-
-    model.compile(loss='binary_crossentropy',
-                  optimizer=optimizer,
+    model.add(Dense(units=units_L2,
+                    activation=activation,
+                    kernel_initializer=initmode,
+                    kernel_regularizer=l2(L2_reg_penalty)))
+    model.add(Dense(units=1,
+                    activation=activation))
+    model.compile(optimizer=optimizer(learning_rate=learnrate),
+                  loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
+
